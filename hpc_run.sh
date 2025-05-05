4c36771125b9d56d7bdde39f2130e47a208a5a5c
Ashfaq Salehin <ashfaq.salehin1701@gmail.com>
2025-05-05 13:35:09 +0100

    Updated bash run

--- hpc_run.sh
+++ hpc_run.sh
@@ -12,5 +12,3 @@ source venv/bin/activate
 pip install -r requirements.txt
 
 python test.py "$@"
-
-# START=$(date +%s); echo "Start Time: $(date +%T)"; python test.py --use_gpu; END=$(date +%s); echo "End Time: $(date +%T)"; echo "Duration: $(printf '%02d:%02d:%02d\n' $(( (END-START)/3600 )) $(( ((END-START)%3600)/60 )) $(( (END-START)%60 )))"
