eff1106acb50b5be8741bba5f94f5569091602cc
Ashfaq Salehin <ashfaq.salehin1701@gmail.com>
2025-05-08 20:32:54 +0100

    Reduced walk count in streaming case

--- test_sliding_window.py
+++ test_sliding_window.py
@@ -121,10 +121,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Temporal Walk Benchmark")
 
     parser.add_argument('--use_gpu', action='store_true', help='Enable GPU acceleration')
-    parser.add_argument('--increment_size', type=int, default=3_000_000,
-                        help='Timestamp range for incremental edge addition (default: 3,000,000)')
-    parser.add_argument('--sliding_window', type=int, default=60_000_000,
-                        help='Sliding window (default: 60,000,000)')
+    parser.add_argument('--increment_size', type=int, default=5_000_000,
+                        help='Timestamp range for incremental edge addition (default: 5,000,000)')
+    parser.add_argument('--sliding_window', type=int, default=70_000_000,
+                        help='Sliding window (default: 70,000,000)')
     parser.add_argument('--walk_count', type=int, default=2_000_000,
                         help='Walk count')
 
