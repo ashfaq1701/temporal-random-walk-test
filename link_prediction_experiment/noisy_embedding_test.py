ca928c08090366040e7dbd18edfa53fc1ec1d890
Ashfaq Salehin <ashfaq.salehin1701@gmail.com>
2025-07-10 04:19:04 +0100

    Trying to speed up training

--- link_prediction_experiment/noisy_embedding_test.py
+++ link_prediction_experiment/noisy_embedding_test.py
@@ -10,6 +10,9 @@ from typing import Dict
 
 import numpy as np
 import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from gensim.models import Word2Vec
 from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score, f1_score
