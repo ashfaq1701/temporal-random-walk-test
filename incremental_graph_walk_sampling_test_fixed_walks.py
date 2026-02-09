d0620255f476679ffda76f0ae74a071449068a39
Ashfaq Salehin <ashfaq.salehin1701@gmail.com>
2026-02-09 14:03:31 +0000

    Fix test script

--- incremental_graph_walk_sampling_test_fixed_walks.py
+++ incremental_graph_walk_sampling_test_fixed_walks.py
@@ -126,26 +126,26 @@ def main(data_file, n_runs):
     print(f"Loaded dataset with {len(data_df):,} edges")
 
     results = {
-        # CPU
-        "walk_time_cpu_tn2v": walk_sampling_vs_edges_simple(
-            data_df, False, "tn2v", n_runs
+        # GPU
+        "walk_time_gpu_index": walk_sampling_vs_edges_simple(
+            data_df, True, "index", n_runs
         ),
+        "walk_time_gpu_weight": walk_sampling_vs_edges_simple(
+            data_df, True, "weight", n_runs
+        ),
+        "walk_time_gpu_tn2v": walk_sampling_vs_edges_simple(
+            data_df, True, "tn2v", n_runs
+        ),
+
+        # CPU
         "walk_time_cpu_index": walk_sampling_vs_edges_simple(
             data_df, False, "index", n_runs
         ),
         "walk_time_cpu_weight": walk_sampling_vs_edges_simple(
             data_df, False, "weight", n_runs
         ),
-
-        # GPU
-        "walk_time_gpu_tn2v": walk_sampling_vs_edges_simple(
-            data_df, True, "tn2v", n_runs
-        ),
-        "walk_time_gpu_index": walk_sampling_vs_edges_simple(
-            data_df, True, "index", n_runs
-        ),
-        "walk_time_gpu_weight": walk_sampling_vs_edges_simple(
-            data_df, True, "weight", n_runs
+        "walk_time_cpu_tn2v": walk_sampling_vs_edges_simple(
+            data_df, False, "tn2v", n_runs
         ),
     }
 
